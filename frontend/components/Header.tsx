15b215a6ac3c114f601a5dcfc3a621752a2c23a9
jamison <jamisonrobey@gmail.com>
2024-05-16 08:12:47 +1000

    fix: use JSX svg attributes

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -9,9 +9,9 @@ export const Header = () => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="w-16 h-16 text-blue-500"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
