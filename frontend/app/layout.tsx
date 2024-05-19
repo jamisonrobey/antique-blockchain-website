f91f5b019ec7f28e57500939186a7a3141769066
jamison <jamisonrobey@gmail.com>
2024-05-19 20:51:25 +1000

    Refactor: moving metamask provider around

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from "next";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+import { Provider } from "@/components/Provider";
 import "./globals.css";
 import { interText } from "@/components/fonts/fonts";
 const metadata: Metadata = {
@@ -15,7 +18,13 @@ export default function RootLayout({
       <body
         className={`${interText.className} mx-auto  grid place-items-center bg-white`}
       >
-        {children}
+        <Provider>
+          <Header />
+          <div className="w-full border-b-2 border-slate-200"></div>
+          {children}
+          <div className="w-full border-t-2 border-slate-200"></div>
+          <Footer />
+        </Provider>
       </body>
     </html>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,6 @@
-import { Footer } from "@/components/Footer";
 import { items } from "./api/results/route";
-import { Provider } from "@/components/Provider";
-import { Header } from "@/components/Header";
 import { Body } from "@/components/Body";
 export default async function Home() {
   const initialResults = items.slice(0, 14);
-  return (
-    <>
-      <Provider>
-        <Header />
-        <div className="w-full border-b-2 border-slate-200"></div>
-        <Body initialResults={initialResults} />
-      </Provider>
-      <div className="w-full border-t-2 border-slate-200"></div>
-      <Footer />
-    </>
-  );
+  return <Body initialResults={initialResults} />;
 }
