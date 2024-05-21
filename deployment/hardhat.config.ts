351230cafeb45f3848a5f3228437a42027c45e82
jamison <jamisonrobey@gmail.com>
2024-05-21 16:34:26 +1000

    refactor: rename directory

---
+++ deployment/hardhat.config.ts
@@ -0,0 +1,7 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { ethers } from "hardhat";
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+export default config;
--- deployment/test/AntiqueCertification.ts
+++ deployment/test/AntiqueCertification.ts
@@ -1,15 +1,24 @@
 import { expect } from "chai";
 import hre from "hardhat";
+import { ethers } from "hardhat";
+import { AntiqueCertification } from "../typechain-types";
+
+const CERTIFICATION_BODY_WALLET =
+  "0xfb172542b405d0685bd29c3f4386159e76d9330b".toLowerCase();
 
 describe("AntiqueCertification", function () {
   it("Constructor", async function () {
-    const antiqueCertificationBodyWalletAddr =
-      "0xfb172542b405d0685bd29c3f4386159e76d9330b".toLowerCase();
+    if (!ethers.isAddress(CERTIFICATION_BODY_WALLET)) {
+      console.error(
+        "\x1b[31mError: CERTIFICATION_BODY_WALLET is not valid\x1b[0m. Check AntiqueCertification.ts"
+      );
+      process.exit(1);
+    }
 
     // deploy an AntiqueCertification contract with this wallet address and check the address stored on the contract
     const antiqueCertification = await hre.ethers.deployContract(
       "AntiqueCertification",
-      [antiqueCertificationBodyWalletAddr]
+      [CERTIFICATION_BODY_WALLET]
     );
 
     // test
@@ -17,6 +26,6 @@ describe("AntiqueCertification", function () {
       await antiqueCertification
         .antiqueCertificationBody()
         .then((addr) => addr.toLowerCase()) // annoying case sensitivity probably better way to do this!
-    ).to.equal(antiqueCertificationBodyWalletAddr);
+    ).to.equal(CERTIFICATION_BODY_WALLET);
   });
 });
